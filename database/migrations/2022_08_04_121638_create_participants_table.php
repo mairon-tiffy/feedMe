88f1d23713dbdc9850f4e5f700ccffd525a95c50
mairon-tiffy <mairon0127@yahoo.co.jp>
2022-08-04 12:46:46 +0900

    participantのマイグレーション/model

---
+++ database/migrations/2022_08_04_121638_create_participants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->integer('event_detail_id');
+            $table->integer('user_id');
+            $table->integer('number');
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+};
